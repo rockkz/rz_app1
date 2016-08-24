663e4326b85ed20334021497d2264f1a3617cdb2
Rockkz <zspmail@mail.ru>
2016-08-24 10:40:07 -0700

    mmy initial repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RzApp1::Application.config.secret_key_base = 'e79f8a128c3575fa30b4c1d09e12a1b88266e712d90595110374df6403a47edced23362c8d1995c6fb74d1ad02b2c2b9d06c2ca2c9a706c020a02e81339fdbb9'
